11f25daeff3e7e6f4b65bbc30d47d7df3b2e9192
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2021-10-27 00:24:30 +0200

    Stub action for automatic updates

--- herd.rb
+++ herd.rb
@@ -1,8 +1,8 @@
 class Herd < Formula
   desc "Massively parallel SSH client"
   homepage "https://herd.seveas.net"
-  url "https://github.com/seveas/herd/archive/refs/tags/v0.7.0.tar.gz"
-  sha256 "5d78810b1e91b282dec508cf3ddc63db767fc184721d918c2506f5cf8a34a69f"
+  url "https://github.com/seveas/herd/archive/refs/tags/v0.x.0.tar.gz"
+  sha256 "5d78810b1e91b282dec508cf3ddc63db767fc184721d918c2506fxcf8a34a69f"
   license "MIT"
 
   depends_on "go" => [:build, :test]
