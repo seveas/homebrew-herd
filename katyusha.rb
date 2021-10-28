26beb6f1bd45c961fad63a76058cf77855798985
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2021-10-28 19:20:56 +0000

    Automated update to v0.7.1

--- katyusha.rb
+++ katyusha.rb
@@ -1,8 +1,8 @@
 class Katyusha < Formula
   desc "Massively parallel SSH client"
   homepage "https://katyusha.seveas.net"
-  url "https://github.com/seveas/katyusha/archive/refs/tags/v0.7.0.tar.gz"
-  sha256 "5d78810b1e91b282dec508cf3ddc63db767fc184721d918c2506f5cf8a34a69f"
+  url "https://github.com/seveas/katyusha/archive/refs/tags/v0.7.1.tar.gz"
+  sha256 "f875c22963c8f16379594b5597e5254441adc3135450060ef44313c97151c882"
   license "MIT"
 
   depends_on "go" => [:build, :test]
