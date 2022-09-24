c1bbe4c9fc80d878abdbc2419de680917e6a7878
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2022-09-24 21:38:47 +0000

    Automated update to v0.9.1

--- herd.rb
+++ herd.rb
@@ -1,8 +1,8 @@
 class Herd < Formula
   desc "Massively parallel SSH client"
   homepage "https://herd.seveas.net"
-  url "https://github.com/seveas/herd/archive/refs/tags/v0.8.1.tar.gz"
-  sha256 "246dd3ff299f990ff3486d61c543c23db2d7e767c564182e6133d631a119f10f"
+  url "https://github.com/seveas/herd/archive/refs/tags/v0.9.1.tar.gz"
+  sha256 "9ec3a1525aa7d2e052d77f704be489c7a61ece4c9aeda93a0b63cc0076ae8a75"
   license "MIT"
 
   depends_on "go" => [:build, :test]
